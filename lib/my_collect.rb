56e0f489a5580139f6a12b23c6be943876c85bb9
Andres Carvajal <jc1993box@hotmail.com>
2019-07-18 03:40:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect (languages)
+  i = 0
+  while i < languages.length
+  
+  yield (languages [i])
+   i += 1
+end
+my_collect(languages) do |language| language.upcase
+end
 
-
+  
+  
+languages = ["ruby", "javascript", "python", "objective-c"]
+my_collect(languages) do |language| language.upcase
+end
